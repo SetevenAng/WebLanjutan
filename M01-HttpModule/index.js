932e3e646faccf0f158c2a46f0154d8b4c881636
Seteven Ang <steven.ang12377@gmail.com>
2021-03-12 23:08:17 +0700

    listen post 3400 formating html

--- M01-HttpModule/index.js
+++ M01-HttpModule/index.js
@@ -7,9 +7,9 @@ http
         res.writeHead(200,{"Content-Type": "text/html"});
 
         //respon ke browser / client
-        res.write("<H1>Module HTTP Mobile and Web</H1>");
+        res.write("<H1>Module HTTP Mobile and Web Dengan Formatting HTML</H1>");
 
         //Respon diakhiri
         res.end();
     })
-    .listen(3000);
+    .listen(3400);
